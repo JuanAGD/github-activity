e824d141bcb5be87d2d0dccaa92a692a76b12a63
JuanAGD <gonzalezdiaz057@gmail.com>
2026-04-08 07:38:13 -0500

    Change empleados to employee

--- src/main/java/com/example/empresa_crud/controllers/EmployeeControlller.java
+++ src/main/java/com/example/empresa_crud/controllers/EmployeeControlller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/empleados")
+@RequestMapping("/api/employee")
 @RequiredArgsConstructor
 public class EmployeeControlller {
 
